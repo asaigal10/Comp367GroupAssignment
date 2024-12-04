0b5b5cd94537bb7df9e32a275fc901126fed9881
asaigal10 <113387099+asaigal10@users.noreply.github.com>
2024-12-03 23:08:37 -0500

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/COMP229-F2022-MidTerm-Start-010/server/config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  //local MongoDB deployment ->
-  //URI: mongodb://127.0.0.1/faculty_info//
-  URI: "mongodb+srv://amrit:raptors43@cluster0.xrklssu.mongodb.net/?retryWrites=true&w=majority"
-};
